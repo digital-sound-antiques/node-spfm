a56ad7d131547a6b5407c84e0454a29ba9924ee8
Mitsutaka Okazaki <okaxaki@users.noreply.github.com>
2019-12-14 18:02:57 +0900

    Improve YM2151 LFRQ adjustment.

--- src/filter/opm-clock-filter.ts
+++ src/filter/opm-clock-filter.ts
@@ -5,23 +5,37 @@ const keyIndexToCode = [0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13, 14];
 
 export class YM2151ClockFilter implements RegisterFilter {
   _ratio: number;
-  _diff: number;
   _regs = new Uint8Array(256);
   _outRegs = new Uint8Array(256);
+  _initialized = false;
+  _keyDiff: number;
+  _lfoDiff = 0;
 
   constructor(inClock: number, outClock: number) {
     this._ratio = inClock / outClock;
-    this._diff = Math.round(12 * Math.log2(1.0 / this._ratio) * 256);
+    this._keyDiff = Math.round(12 * Math.log2(1.0 / this._ratio) * 256);
+
+    /* LFO_FREQ = CLOCK * POWER(2, LFRQ/16 - 32) = CLOCK' * POWER(2, LFRQ'/16 - 32) */
+    /* => LFRQ' = 16 * LOG2(CLOCK/CLOCK') + LFRQ */
+    this._lfoDiff = Math.round(16 * Math.log2(1.0 / this._ratio));
   }
   filterReg(context: any, data: RegisterData): RegisterData[] {
     if (this._ratio !== 1.0 && data.a != null) {
+      const result = Array<RegisterData>();
+
+      if (!this._initialized) {
+        const lfrq = Math.max(0, Math.min(255, this._lfoDiff));
+        result.push({ port: data.port, a: data.a, d: lfrq });
+        this._initialized = true;
+      }
+
       this._regs[data.a] = data.d;
       if ((0x28 <= data.a && data.a <= 0x2f) || (0x30 <= data.a && data.a <= 0x37)) {
         const ch = data.a - (data.a < 0x30 ? 0x28 : 0x30);
         const orgKeyIndex = keyCodeToIndex[this._regs[0x28 + ch] & 0xf];
         const orgKey = (orgKeyIndex << 8) | this._regs[0x28 + ch];
         let octave = (this._regs[0x28 + ch] >> 4) & 0x7;
-        let newKey = orgKey + this._diff;
+        let newKey = orgKey + this._keyDiff;
         if (newKey < 0) {
           if (0 < octave) {
             octave--;
@@ -37,7 +51,6 @@ export class YM2151ClockFilter implements RegisterFilter {
             newKey = (12 << 8) - 1;
           }
         }
-        const result = [];
         const okc = (octave << 4) | keyIndexToCode[newKey >> 8];
         if (okc != this._outRegs[0x28 + ch]) {
           result.push({ port: data.port, a: 0x28 + ch, d: okc });
@@ -51,12 +64,12 @@ export class YM2151ClockFilter implements RegisterFilter {
         return result;
       } else if (data.a === 0x0f) {
         const nfrq = Math.min(0x1f, Math.round((data.d & 0x1f) * this._ratio));
-        return [{ port: data.port, a: data.a, d: (data.d & 0xe0) | nfrq }];
+        result.push({ port: data.port, a: data.a, d: (data.d & 0xe0) | nfrq });
+        return result;
       } else if (data.a === 0x18) {
-        const counter_add = 0x10 + (data.d & 0xf);
-        const new_counter_add = Math.max(0x10, Math.min(0x1f, counter_add * this._ratio));
-        const lfrq = (data.d & 0xf0) | (new_counter_add - 0x10);
-        return [{ port: data.port, a: data.a, d: lfrq }];
+        const lfrq = Math.max(0, Math.min(255, Math.round(this._lfoDiff + data.d)));
+        result.push({ port: data.port, a: data.a, d: lfrq });
+        return result;
       }
     }
     return [data];
