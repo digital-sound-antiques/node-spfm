80cce5fe2f3ddd9d40b5128f841880028e88fb31
Mitsutaka Okazaki <okaxaki@users.noreply.github.com>
2019-12-16 05:13:02 +0900

    Support OPLx clock conversion for more than 2x clock ratio.

--- src/filter/opl-clock-filter.ts
+++ src/filter/opl-clock-filter.ts
@@ -3,8 +3,12 @@ import { RegisterFilter, RegisterData } from "./register-filter";
 export class YM3526ClockFilter implements RegisterFilter {
   _ratio: number;
   _regs = new Uint8Array(256);
+  _outRegs = new Uint8Array(256);
   constructor(inClock: number, outClock: number) {
     this._ratio = inClock / outClock;
+    for (let i = 0; i < this._outRegs.length; i++) {
+      this._outRegs[i] = -1;
+    }
   }
   filterReg(context: any, data: RegisterData): RegisterData[] {
     if (this._ratio !== 1.0 && data.a != null) {
@@ -13,13 +17,28 @@ export class YM3526ClockFilter implements RegisterFilter {
         const al = data.a & 0xaf;
         const ah = (data.a & 0xaf) + 0x10;
         const fnum = ((this._regs[ah] & 3) << 8) | this._regs[al];
-        const new_fnum = Math.min(0x3ff, fnum / this._ratio);
+        let new_fnum = Math.round(fnum / this._ratio);
+        let new_blk = (this._regs[ah] & 0x1c) >> 2;
+        while (new_fnum > 0x3ff) {
+          new_fnum >>= 1;
+          new_blk++;
+        }
+        if (new_blk > 7) {
+          new_blk = 7;
+          new_fnum = 0x3ff;
+        }
         const dl = new_fnum & 0xff;
-        const dh = (this._regs[ah] & 0xfc) | (new_fnum >> 8);
-        return [
-          { port: data.port, a: ah, d: dh },
-          { port: data.port, a: al, d: dl }
-        ];
+        const dh = (this._regs[ah] & 0xe0) | (new_blk << 2) | (new_fnum >> 8);
+        const result = [];
+        if (this._outRegs[ah] != dh) {
+          result.push({ port: data.port, a: ah, d: dh });
+          this._outRegs[ah] = dh;
+        }
+        if (this._outRegs[al] != dl) {
+          result.push({ port: data.port, a: al, d: dl });
+          this._outRegs[al] = dl;
+        }
+        return result;
       }
     }
     return [data];
@@ -29,8 +48,12 @@ export class YM3526ClockFilter implements RegisterFilter {
 export class YM2413ClockFilter implements RegisterFilter {
   _ratio: number;
   _regs = new Uint8Array(256);
+  _outRegs = new Uint8Array(256);
   constructor(inClock: number, outClock: number) {
     this._ratio = inClock / outClock;
+    for (let i = 0; i < this._outRegs.length; i++) {
+      this._outRegs[i] = -1;
+    }
   }
   filterReg(context: any, data: RegisterData): RegisterData[] {
     if (this._ratio !== 1.0 && data.a != null) {
@@ -39,13 +62,28 @@ export class YM2413ClockFilter implements RegisterFilter {
         const al = (data.a & 0xf) + 0x10;
         const ah = (data.a & 0xf) + 0x20;
         const fnum = ((this._regs[ah] & 1) << 8) | this._regs[al];
-        const new_fnum = Math.min(0x1ff, fnum / this._ratio);
+        let new_fnum = Math.round(fnum / this._ratio);
+        let new_blk = (this._regs[ah] & 0xe) >> 1;
+        while (new_fnum > 0x1ff) {
+          new_fnum >>= 1;
+          new_blk++;
+        }
+        if (new_blk > 7) {
+          new_blk = 7;
+          new_fnum = 0x1ff;
+        }
         const dl = new_fnum & 0xff;
-        const dh = (this._regs[ah] & 0xfe) | (new_fnum >> 8);
-        return [
-          { port: data.port, a: ah, d: dh },
-          { port: data.port, a: al, d: dl }
-        ];
+        const dh = (this._regs[ah] & 0xf0) | (new_blk << 1) | (new_fnum >> 8);
+        const result = [];
+        if (this._outRegs[ah] != dh) {
+          result.push({ port: data.port, a: ah, d: dh });
+          this._outRegs[ah] = dh;
+        }
+        if (this._outRegs[al] != dl) {
+          result.push({ port: data.port, a: al, d: dl });
+          this._outRegs[al] = dl;
+        }
+        return result;
       }
     }
     return [data];
